075d7ecc7d50f9da091301c2ec0d92c8d302b6e4
emma773 <emmahorton03@gmail.com>
2024-09-19 11:03:58 +0100

    add correct connection string to config file

--- config-db.js
+++ config-db.js
@@ -1,12 +1,18 @@
 (function() {
-	const db_info = {url:'localhost',
-                        username: 'webuser',
-                        password: 'webuser',
-                        port: '24552',
-						database: 'mainDatabase',
-                        collection: 'createRuns',
-                        users: 'users'
-                    };
+	// const db_info = {url:'localhost',
+    //                     username: 'webuser',
+    //                     password: 'webuser',
+    //                     port: '24552',
+	// 					database: 'mainDatabase',
+    //                     collection: 'createRuns',
+    //                     users: 'users'
+    //                 };
+    const db_info = {
+        url: `mongodb+srv://emmahorton03:wc8zzJKOdmpMDxE1@reunify.50cg9.mongodb.net/?retryWrites=true&w=majority&appName=reunify`,
+        database: 'mainDatabase',
+        collection: 'createRuns',
+        users: 'users'
+    };
 
 	const moduleExports = db_info;
 
